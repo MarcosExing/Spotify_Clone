04dc8e36df4461e36a3bbbebc506acda45882dab
Marcos Exing <95306754+MarcosExing@users.noreply.github.com>
2024-04-06 02:02:21 -0300

    Deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import './fonts.css';
 import './vars.css';
 import './App.css';
 import './media-queries.css';
-import 'https://kit.fontawesome.com/8da7e8955d.js';
 import Header from './Header/Header';
 import SideBar from './SideBar/SideBar';
 import Content from './Content/Content';
